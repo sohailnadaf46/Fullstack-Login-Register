85456debe78dd43171e2b1ece97a1e9e8d457eaf
Sohail Nadaf <sohailnadaf967@gmail.com>
2024-05-02 15:32:14 +0530

    connected mongoGB database

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import mongoose from "mongoose";
 import { router } from "./routes/authRoutes.js";
+
+
+dotenv.config();
+//database connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("database connected"))
+  .catch((error) => console.error("Error connecting to database:", error.message));
+
 
 const app = express();
 
